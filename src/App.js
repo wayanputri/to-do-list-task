0e7b66c8205aa6ab891a50b0ccaa9db818208e3e
wayanputri <wayanputriyanti1502@gmail.com>
2023-09-23 19:48:09 +0700

    to do list task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Task from "./Task";
+import TaskForm from "./TaskForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [tasks, setTasks] = useState([]);
+	useEffect(() => {
+		if (tasks.length === 0) return;
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+	}, [tasks]);
+	useEffect(() => {
+		const tasks = JSON.parse(localStorage.getItem("tasks"));
+		setTasks(tasks);
+	}, []);
+	function addTasks(name) {
+		setTasks((prev) => {
+			return [...prev, { name: name, done: false }];
+		});
+	}
+	function removeTask(indexToRemove) {
+		setTasks((prev) => {
+			return prev.filter((taskObject, index) => index !== indexToRemove);
+		});
+	}
+	function updateTaskDone(taskIndex, newDone) {
+		setTasks((prev) => {
+			const newTasks = [...prev];
+			newTasks[taskIndex].done = newDone;
+			return newTasks;
+		});
+	}
+	const numberComplete = tasks.filter((t) => t.done).length;
+	const numberTotal = tasks.length;
+
+	function getMessage() {
+		const percentage = (numberComplete / numberTotal) * 100;
+		if (percentage === 0) {
+			return "Try to do least one!🙏";
+		}
+		if (percentage === 100) {
+			return "Nice job for today!✨";
+		}
+		return "Keep it going 💪";
+	}
+	function renameTask(index, newName) {
+		setTasks((prev) => {
+			const newTasks = [...prev];
+			newTasks[index].name = newName;
+			return newTasks;
+		});
+	}
+
+	return (
+		<main>
+			<h1>
+				{numberComplete}/{numberTotal} Complete
+			</h1>
+			<h2>{getMessage()}</h2>
+			<TaskForm onAdd={addTasks} />
+			{tasks.map((task, index) => (
+				<Task
+					{...task}
+					onRename={(newName) => renameTask(index, newName)}
+					onTrash={() => removeTask(index)}
+					onToggle={(done) => updateTaskDone(index, done)}
+				/>
+			))}
+		</main>
+	);
 }
 
 export default App;
